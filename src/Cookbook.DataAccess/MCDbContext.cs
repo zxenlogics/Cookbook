fb9aa5614f160a11a7a089e5a592afce8f362d25
Denzil N. Brown <denzilnbrown@gmail.com>
2017-06-08 09:42:11 -0400

    'updates'

--- src/Cookbook.DataAccess/MCDbContext.cs
+++ src/Cookbook.DataAccess/MCDbContext.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Annotations;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
 using MC.Internal.DevOps.Entities;
@@ -19,11 +22,31 @@ namespace MC.Internal.DevOps.MCDataAccess
 
 
 
+            //modelBuilder.Entity<Environment>()
+            //    .HasRequired<Project>(p => p.Project)
+            //    .WithMany(p => p.Environments)
+            //    .HasForeignKey(p => p.ProjectId);
+
             modelBuilder.Entity<Environment>()
-                .HasOptional(p => p.Project);
-         
-            modelBuilder.Entity<Project>()
-                .HasRequired(x => x.Environments);
+                .HasOptional(p => p.Project)
+                .WithMany(p => p.Environments);
+
+
+            modelBuilder.Entity<EnvironmentDatabase>()
+                .HasKey(edb => new {edb.EnvironmentId, edb.DatabaseId});
+            modelBuilder.Entity<EnvironmentDatabase>()
+                .HasRequired(d => d.Environment)
+                .WithMany(d => d.Databases);
+            modelBuilder.Entity<EnvironmentDatabase>()
+                .HasRequired(d => d.Database);
+            //modelBuilder.Entity<EnvironmentDatabase>()
+            //    .Property(d => d.DatabaseId)
+            //    .IsRequired()
+            //    .HasColumnAnnotation("Index",
+            //        new IndexAnnotation(new IndexAttribute("IX_DatabaseId") { IsUnique = true }));
+
+
+
 
             //modelBuilder.Entity<Blog>()
             //.Property(b => b.Created)
@@ -44,6 +67,6 @@ namespace MC.Internal.DevOps.MCDataAccess
         public DbSet<MC.Internal.DevOps.Entities.Database> Databases { get; set; }
         public DbSet<Client> Clients { get; set; }
         public DbSet<Branch> Branches { get; set; }
-        //public DbSet<EnvironmentDatabase> EnvironmentDatabases { get; set; }
+        public DbSet<EnvironmentDatabase> EnvironmentDatabases { get; set; }
     }
 }
